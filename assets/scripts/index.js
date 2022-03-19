765c1195d8fe534bbe35184e3d5ff3e1baf5e6f1
Sanjay Prajapati <sanjay270899@gmail.com>
2022-03-19 15:12:58 +0530

    ✨ Add loader

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,17 @@
 import { NavComponent } from "../components/nav.js";
 import { FooterComponent } from "../components/footer.js";
 
+// Loader
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector("#loader").style.visibility = "visible";
+  } else {
+    document.querySelector("#loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+  }
+};
+
 // Adding nav
 const navRoot = document.getElementById("nav-root");
 navRoot.innerHTML = NavComponent;
