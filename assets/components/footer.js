056bb0c1a5f924de552be746ad37e85be856cb4e
Sanjay Prajapati <sanjay270899@gmail.com>
2022-03-19 13:59:49 +0530

    ✨ Hero and footer

---
+++ assets/components/footer.js
@@ -0,0 +1,25 @@
+export const FooterComponent = `<footer>
+        <div class="footer-section-container">
+          <div class="footer-section">
+            <h3>Quick Links</h3>
+            <ul>
+              <li><a href="/about.html">About Us</a></li>
+              <li><a href="/contact.html">Contact Us</a></li>
+              <li><a href="/members.html">Members</a></li>
+            </ul>
+          </div>
+          <div class="footer-section">
+            <h3>Newsletter</h3>
+            <p>
+              Subscribe to our newsletter to get your weekly dose of
+              news,updates,tips and special offers.
+            </p>
+            <div class="footer-section-row">
+              <input type="email" placeholder="Enter your email address" />
+              <button>Subscribe</button>
+            </div>
+          </div>
+        </div>
+        <hr />
+        <p class="copy-right">© 2022 Sanjay Prajapati</p>
+      </footer>`;
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,5 +1,10 @@
-import { NavComponent } from "../elements/nav.js";
+import { NavComponent } from "../components/nav.js";
+import { FooterComponent } from "../components/footer.js";
 
 // Adding nav
 const navRoot = document.getElementById("nav-root");
 navRoot.innerHTML = NavComponent;
+
+// Adding footer
+const footerRoot = document.getElementById("footer-root");
+footerRoot.innerHTML = FooterComponent;
