05ad04540aa0f741bec94d0d3620d0ee61f01d4f
Sanjay Prajapati <sanjay270899@gmail.com>
2022-03-19 13:19:27 +0530

    ✨ Add nav

---
+++ assets/elements/nav.js
@@ -0,0 +1,19 @@
+export const NavComponent = `<nav>
+    <div class="left">
+      <a href="./"> Avengers Fan Club </a>
+    </div>
+    <div class="right">
+      <a href="./about.html" class="nav-item">
+        About Us
+      </a>
+      <a href="./members.html" class="nav-item">
+        Members
+      </a>
+      <a href="./contact.html" class="nav-item">
+        Contact Us
+      </a>
+      <a href="./join.html" class="nav-item nav-item-button">
+        Join Club
+      </a>
+    </div>
+  </nav>`;
---
+++ assets/scripts/index.js
@@ -0,0 +1,5 @@
+import { NavComponent } from "../elements/nav.js";
+
+// Adding nav
+const navRoot = document.getElementById("nav-root");
+navRoot.innerHTML = NavComponent;
